6fc89f7de2a730319b97cfa4f30eee99c5a394da
vivek <vivekkashyap043@gmail.com>
2024-10-15 21:01:10 +0530

    changed

--- src/components/ApiRequest.js
+++ src/components/ApiRequest.js
@@ -2,7 +2,6 @@ import {BASE_APP_URL, APP_OWNER_NAME, APP_LINK_NAME} from '@env';
 import {Alert} from 'react-native';
 
 export const getDataWithInt = async (reportName, criteria, value, token) => {
-  console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&called")
   try {
     const url = `${BASE_APP_URL}/${APP_OWNER_NAME}/${APP_LINK_NAME}/report/${reportName}?criteria=${criteria}==${value}`;
     console.log('url in getDataWithInt: ', url);
