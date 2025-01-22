de1340e2c3d9c471c3e15ce7943c31850f315554
Sasivenkatamurthy <sasivenkatamurthyk@gmail.com>
2025-01-22 10:51:19 +0530

    Merge branch 'main' of https://github.com/mahidhar9/SathyaSaiGrama into UnitTesting

--- __tests__/L1 Employee Test cases/Account/ValidateTheEditButtonInPersonalInfoSection.test.js
+++ __tests__/L1 Employee Test cases/Account/ValidateTheEditButtonInPersonalInfoSection.test.js
@@ -197,9 +197,8 @@
 
 
 
-
 import React from 'react';
-import {render, fireEvent,cleanup, waitFor, act} from '@testing-library/react-native';
+import {render, fireEvent, waitFor, act} from '@testing-library/react-native';
 import Edit from '../../../src/screens/Edit'; // Adjust the import path as necessary
 import UserContext from '../../../context/UserContext';
 import {AuthContext} from '../../../src/auth/AuthProvider';
@@ -216,7 +215,9 @@ jest.mock('react-native-phone-number-input', () => ({
 }));
 
 jest.mock('../../../src/components/ApiRequest', () => ({
- patchDataWithInt: jest.fn(),
+  patchDataWithInt: jest.fn().mockResolvedValue({
+    result: [{code: 3000}],
+  }),
 }));
 
 jest.mock('react-native/Libraries/Alert/Alert', () => ({
@@ -226,116 +227,150 @@ jest.mock('react-native/Libraries/Alert/Alert', () => ({
 const mockNavigation = {navigate: jest.fn()};
 
 const mockUserContextValue = {
-  dapartment: 'Engineering',       
-  dapartmentExists: true,            
-  getAccessToken: jest.fn(),
-  familyMembersData: [{
-    Building: 'Building A',
-    Department: { ID: 'd1' },
-    Flat: 'Flat 101',
-    ID: 'family123',
-  }],
+  userType: 'admin',
+  setUserType: jest.fn(),
+  accessToken: 'mockAccessToken123',
+  setUserEmail: jest.fn(),
+  setL1ID: jest.fn(),
+  loggedUser: {
+    name: 'John Doe',
+    userId: 1, // Ensure userId is present
+    L1ID: 1, // Add L1ID as per your context
+    // Include other necessary fields if required
+  },
+  setLoggedUser: jest.fn(),
+  deviceToken: 'mockDeviceToken456',
+  resident: {id: 'r123', name: 'Jane Resident'},
+  setResident: jest.fn(),
+  setProfileImage: jest.fn(),
+  employee: {id: 'e789', name: 'Alice Employee'},
+  setEmployee: jest.fn(),
+  testResident: {id: 't987', name: 'Test Resident'},
+  setTestResident: jest.fn(),
+  departmentIds: ['d1', 'd2', 'd3'],
+  setDepartmentIds: jest.fn(),
+  dapartment: 'Engineering', // Added dapartment
+  dapartmentExists: true, // Added dapartmentExists
+  familyMembersData: [
+    // Added familyMembersData
+    {
+      ID: 'family123',
+      Building: 'Building A',
+      Department: {ID: 'd1'},
+      Flat: 'Flat 101',
+      Relationship_with_the_primary_contact: 'Sibling',
+      App_User_lookup: {Name_field: 'Jane Doe'},
+    },
+  ],
   flat: {
     building: 'Building A',
     flat: 'Flat 101',
   },
   flatExists: true,
-  flatid: 'flat123',
-  userInfo: [{
-    Email: 'johndoe@example.com',
-    Gender: 'Male',
-    ID: 'user123',
-    Name_field: 'John Doe',
-    Secondary_Phone: '0987654321',
-  }],
-  vehicleInfo: [{
-    ID: 'vehicle1',
-    Vehicle_Number: 'KA 01 AB 1234',
-    Vehicle_Type: '2-Wheeler',
-    dapartment: 'Engineering',
-    dapartmentExists: true,
-    vehicledata: [{
-      ID: 'vehicle1',
-      Vehicle_Number: 'KA 01 AB 1234',
-      Vehicle_Type: '2-Wheeler',
-    }],
-  }],
+  flatMember: false,
+  setFlatMember: jest.fn(),
+  setFlatExists: jest.fn(),
   setDapartment: jest.fn(),
   setDapartmentExists: jest.fn(),
   setFamilyMembersData: jest.fn(),
   setFlat: jest.fn(),
-  setFlatExists: jest.fn(),
-  setFlatMember: jest.fn(),
-  setLoggedUser: jest.fn(),
-  setUserInfo: jest.fn(),
+  getAccessToken: jest.fn(),
 };
+
 const mockAuthContextValue = {
   user: {uid: '123', email: 'mockuser@example.com'},
   setUser: jest.fn(),
 };
-
+const initialVehicleData = [
+  {
+    ID: 'vehicle1',
+    Vehicle_Type: '2-Wheeler',
+    Vehicle_Number: 'KA 01 AB 1234',
+    dapartment: 'Engineering',
+    dapartmentExists: true,
+    vehicledata: [
+      {
+        ID: 'vehicle1',
+        Vehicle_Type: '2-Wheeler',
+        Vehicle_Number: 'KA 01 AB 1234',
+      },
+    ],
+  },
+];
 describe('Edit Screen - Update Vehicle Information', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-  afterEach(() => {
-    cleanup();
-  });
-  it('should update personal information and navigate to MyProfile screen', async () => {
-    patchDataWithInt
-    .mockResolvedValueOnce({ result: [{ message: 'Data Updated Successfully' }] })
-    .mockResolvedValueOnce({ result: [{ message: 'Data Updated Successfully' }] });
 
+  it('should update vehicle information and navigate to MyProfile screen', async () => {
     const {getByText, findByText, debug, getByTestId, getByDisplayValue} =
       render(
         <AuthContext.Provider value={mockAuthContextValue}>
           <UserContext.Provider value={mockUserContextValue}>
             <NavigationContainer>
-            <Edit
-              navigation={mockNavigation}
-              route={{
-                params: {
-                  formType: 'MemberBasicInfo',
-                  userdata: {
-                    ID: 'user123',
-                    Name_field: 'John Doe',
-                    Secondary_Phone: '0987654321',
-                    Email: 'johndoe@example.com',
-                    Gender: 'Male',
-                  },
-                  memberdata: {
-                    App_User_lookup: {
+              <Edit
+                navigation={mockNavigation}
+                route={{
+                  params: {
+                    formType: 'MemberBasicInfo',
+                    userdata: {
+                      ID: 'user123',
                       Name_field: 'John Doe',
+                      Phone_Number: '0987654321',
                       Email: 'johndoe@example.com',
-                      Phone_Number: '1234567890',
-                      ID: 'resident123',
-                      Gender: 'Female',
+                      Gender: 'Male',
                     },
-                    Relationship_with_the_primary_contact: 'Sibling',
+                    vehicledata: initialVehicleData,
+                    familyMembersData: [],
+                    flatExists: true,
+                    flat: {},
                   },
-                },
-              }}
-            />
+                }}
+              />
             </NavigationContainer>
           </UserContext.Provider>
         </AuthContext.Provider>,
       );
+ fireEvent.changeText(getByTestId('gender'), 'Female');
     await act(async () => {
-      expect(mockNavigation.navigate).toHaveBeenCalledWith('MyProfile', {
-        userInfo: {
-          ID: 'user123',
-          Name_field: 'John Doe',
-          Secondary_Phone: '0987654321',
-          Email: 'johndoe@example.com',
-          Gender: 'Male',
-        },
-        vehicleInfo: mockUserContextValue.vehicleInfo,
-        familyMembersData: mockUserContextValue.familyMembersData,
-        flatExists: mockUserContextValue.flatExists,
-        flat: mockUserContextValue.flat,
-        dapartment: mockUserContextValue.dapartment,
-        dapartmentExists: mockUserContextValue.dapartmentExists,
-      });
+      fireEvent.press(getByText('Submit'));
+    });
+
+    debug();
+    await waitFor(() => {
+      expect(mockNavigation.navigate).toHaveBeenCalledWith('MyProfile',{
+        "dapartment": undefined,
+        "dapartmentExists": undefined,
+        "familyMembersData": undefined,
+        "flat": undefined,
+        "flatExists": {},
+        "flatMember": undefined,
+        "userInfo": [
+          {
+            "Email": "johndoe@example.com",
+            "Gender": "Male",
+            "ID": "user123",
+            "Name_field": "John Doe",
+            "Phone_Number": "0987654321"
+          }
+        ],
+        "vehicleInfo": [
+          {
+            "ID": "vehicle1",
+            "Vehicle_Number": "KA 01 AB 1234",
+            "Vehicle_Type": "2-Wheeler",
+            "dapartment": "Engineering",
+            "dapartmentExists": true,
+            "vehicledata": [
+              {
+                "ID": "vehicle1",
+                "Vehicle_Number": "KA 01 AB 1234",
+                "Vehicle_Type": "2-Wheeler"
+              }
+            ]
+          }
+        ]
+      })
     });
   });
 });
--- __tests__/L1 Employee Test cases/Pending Tab/L1AbleToEditAndUpdateTheVisitorInformation.test.js
+++ __tests__/L1 Employee Test cases/Pending Tab/L1AbleToEditAndUpdateTheVisitorInformation.test.js
@@ -187,23 +187,14 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import React from 'react';
-// import {render,fireEvent,waitFor,act,cleanup,} from '@testing-library/react-native';
+// import {
+//   render,
+//   fireEvent,
+//   waitFor,
+//   act,
+//   cleanup,
+// } from '@testing-library/react-native';
 // import { NavigationContainer } from '@react-navigation/native';
 // import EditVerifyDetails from '../../../src/screens/approval/EditVerifydetails';
 // import { updateRecord } from '../../../src/screens/approval/VerifyDetails';
@@ -211,6 +202,17 @@
 // import { AuthContext } from '../../../src/auth/AuthProvider';
 // import { Alert } from 'react-native';
 // import DatePicker, { getFormatedDate } from 'react-native-modern-datepicker';
+// import RNFS from 'react-native-fs';
+// import Share from 'react-native-share';
+
+// jest.mock('react-native-fs', () => ({
+//   RNFS: {
+//     writeFile: jest.fn(),
+//   }
+// }));
+// jest.mock('react-native-share', () => ({
+//   open: jest.fn(),
+// }));
 
 // jest.mock('react-native-modern-datepicker', () => {
 //   const React = require('react');
@@ -228,155 +230,210 @@
 // });
 
 // jest.mock('../../../src/screens/approval/VerifyDetails', () => ({
-//   updateRecord: jest.fn(() => Promise.resolve({})),
+//   updateRecord: jest.fn(() => Promise.resolve({ result: [{ code: 3000 }] })),
 // }));
 
 // jest.mock('../../../src/components/ApiRequest', () => ({
 //   findDeviceToken: jest.fn().mockResolvedValue('mockDeviceToken123'),
 //   updateDeviceToken: jest.fn().mockResolvedValue({}),
+  
 // }));
+// jest.spyOn(Alert, 'alert').mockImplementation(() => {});
 
 // global.fetch = jest.fn(() =>
 //   Promise.resolve({
-//       ok: true,
-//       json: () => Promise.resolve({responseData: {code: 3000}}),
+//     ok: true,
+//     json: () => Promise.resolve({ responseData: { code: 3000 } }),
 //   })
 // );
-//   const mockNavigation = { navigate: jest.fn() };
 
-//   const mockAuthContextValue = {
-//     user: { email: 'mockuser@example.com' },
-//     setUser: jest.fn(),
-//   };
-
-//   const mockRoute = {
-//     params: {
-//       triggerDialog: true,
-//       code: '3000',
-//       user: {
-//         ID: 'user123',
-//         Referrer_Approval: 'PENDING APPROVAL',
-//         Name_field: { zc_display_value: 'John Doe' },
-//         Phone_Number: '9182590940',
-//         Single_or_Group_Visit: 'Single',
-//         Gender: 'Male',
-//         Guest_Category: 'Visitor',
-//         Priority: 'P3',
-//         Remarks: 'Test remarks',
-//         Date_of_Visit: '2023-10-10',
-//         Vehicle_Information: [
-//           { ID: 'veh1', Vehicle_Type: 'Car', Vehicle_Number: 'KA01CU1234' },
-//         ],
-//         Home_or_Office: 'Office',
-//         Number_of_Men: '1',
-//         Number_of_Women: '0',
-//         Number_of_Boys: '0',
-//         Number_of_Girls: '0',
-//         Number_of_People: 1,
-//       },
+// const mockNavigation = { navigate: jest.fn() };
+// const mockUserContextValue = {
+//   user: {
+//     ID: 'user123',
+//     Referrer_Approval: 'PENDING APPROVAL',
+//   },
+//   setUser: jest.fn(),
+//   userType: 'admin',
+//   setUserType: jest.fn(),
+//   dapartment: 'Engineering',
+//   dapartmentExists: true,
+//   familyMembersData: [
+//     {
+//       ID: 'family123',
+//       Building: 'Building A',
+//       Department: { ID: 'd1' },
+//       Flat: 'Flat 101',
+//       Relationship_with_the_primary_contact: 'Sibling',
+//       App_User_lookup: { Name_field: 'Jane Doe' },
 //     },
-//   };
-  
-// describe('L1 Able to Edit and Update the Visitor Information', () => {
-//   const BASE_APP_URL = 'https://creator.zoho.com/api/v2.1';
-//   const APP_OWNER_NAME = 'annapoornaapp';
-//   const APP_LINK_NAME = 'ashram-visitor-management';
+//   ],
+//   flat: {
+//     building: 'Building A',
+//     flat: 'Flat 101',
+//   },
+//   flatExists: true,
+//   flatMember: false,
+//   setFlatMember: jest.fn(),
+//   setFlatExists: jest.fn(),
+//   setDapartment: jest.fn(),
+//   setDapartmentExists: jest.fn(),
+//   setFamilyMembersData: jest.fn(),
+//   getAccessToken: jest.fn().mockResolvedValue('mockAccessToken123'),
+// };
+// const mockAuthContextValue = {
+//   user: { email: 'mockuser@example.com' },
+//   setUser: jest.fn(),
+// };
 
+// const mockRoute = {
+//   params: {
+//     triggerDialog: true,
+//     code: '3000',
+//     user: {
+//       ID: 'user123',
+//       Referrer_Approval: 'PENDING APPROVAL',
+//       Name_field: { zc_display_value: 'John Doe' },
+//       Phone_Number: '9182590940',
+//       Single_or_Group_Visit: 'Single',
+//       Gender: 'Male',
+//       Guest_Category: 'Visitor',
+//       Priority: 'P3',
+//       Remarks: 'Test remarks',
+//       Date_of_Visit: '2023-10-10',
+//       Vehicle_Information: [
+//         { ID: 'veh1', Vehicle_Type: 'Car', Vehicle_Number: 'KA01CU1234' },
+//       ],
+//       Home_or_Office: 'Office',
+//       Number_of_Men: '1',
+//       Number_of_Women: '0',
+//       Number_of_Boys: '0',
+//       Number_of_Girls: '0',
+//       Number_of_People: 1,
+//     },
+//   },
+// };
+
+// describe('L1 Able to Edit and Update the Visitor Information', () => {
 //   beforeEach(() => {
 //     jest.useFakeTimers();
 
-//     // Reset mock functions before each test
-//     jest.clearAllMocks();
-
-//     // Mock updateRecord to return a successful response
-//     updateRecord.mockResolvedValue({
-//       result: [{ code: 3000 }],
+//     // Mock the global fetch function
+//     global.fetch = jest.fn().mockResolvedValue({
+//       ok: true,
+//       json: jest.fn().mockResolvedValue({ responseData: { code: 3000 } }),
 //     });
 //   });
 
 //   afterEach(() => {
 //     jest.useRealTimers();
+//     jest.resetAllMocks();
 //   });
-
 //   test('Verify that L1 can edit and update visitor information', async () => {
-//     const mockUserContextValue = {
-//       user: {
-//         ID: 'user123',
-//         Referrer_Approval: 'PENDING APPROVAL',
-//       },
-//       setUser: jest.fn(),
-//       userType: 'admin',
-//       setUserType: jest.fn(),
-//       getAccessToken: jest.fn().mockResolvedValue('mockAccessToken123'),
-//       dapartment: 'Engineering',
-//       dapartmentExists: true,
-//       familyMembersData: [
-//         {
-//           ID: 'family123',
-//           Building: 'Building A',
-//           Department: { ID: 'd1' },
-//           Flat: 'Flat 101',
-//           Relationship_with_the_primary_contact: 'Sibling',
-//           App_User_lookup: { Name_field: 'Jane Doe' },
-//         },
-//       ],
-//       flat: {
-//         building: 'Building A',
-//         flat: 'Flat 101',
-//       },
-//       flatExists: true,
-//       flatMember: false,
-//       setFlatMember: jest.fn(),
-//       setFlatExists: jest.fn(),
-//       setDapartment: jest.fn(),
-//       setDapartmentExists: jest.fn(),
-//       setFamilyMembersData: jest.fn(),
-//       setFlat: jest.fn(),
-//     };
-    
+   
 
-//     const { getByText, getByTestId } = render(
+//     const { getByTestId } = render(
 //       <UserContext.Provider value={mockUserContextValue}>
 //         <AuthContext.Provider value={mockAuthContextValue}>
 //           <NavigationContainer>
 //             <EditVerifyDetails
 //               navigation={mockNavigation}
 //               route={mockRoute}
-//               updateLoading={false}
+//               // updateLoading={false}
 //             />
 //           </NavigationContainer>
 //         </AuthContext.Provider>
 //       </UserContext.Provider>
 //     );
 
-//     fireEvent.changeText(getByText('Guest Category'), 'Corporate');
-//     fireEvent.changeText(getByText('Gender'), 'Male');
-//     fireEvent.changeText(getByText('Priority'), 'P1');
-//     fireEvent.changeText(getByText('Remark'), 'Updated remarks');
-//     fireEvent.press(getByText('Update'));
+//     // Simulate user interactions
+//     fireEvent.changeText(getByTestId('GuestCategory'), 'Corporate');
+//     fireEvent.changeText(getByTestId('priorityInput'), 'P1');
+//     fireEvent.changeText(getByTestId('remarkInput'), 'Updated remarks');
+//     fireEvent.press(getByTestId('updateButton'));
+
+//     // Advance timers to handle setTimeout callbacks
 //     act(() => {
 //       jest.advanceTimersByTime(2000);
 //     });
 
-//     // Await the async operations
 //     await waitFor(() => {
-//       expect(updateRecord).toHaveBeenCalledWith(
-//         'Approval_to_Visitor_Report',
-//         {
-//           criteria: 'ID==user123',
-//           data: {
-//             Guest_Category: 'Corporate',
-//             Gender: 'Male',
-//             Priority: 'P1',
-//             Remarks: 'Updated remarks',
-//           },
+//       const BASE_APP_URL = 'https://creator.zoho.com/api/v2.1';
+//       const APP_OWNER_NAME = 'annapoornaapp';
+//       const APP_LINK_NAME = 'ashram-visitor-management';
+//       const REPORT_NAME = 'Approval_to_Visitor_Report';
+//       const USER_ID = 'user123';
+//       const expectedUrl = `${BASE_APP_URL}/${APP_OWNER_NAME}/${APP_LINK_NAME}/report/${REPORT_NAME}/${USER_ID}`;
+
+//       const expectedBody = {
+//         criteria: `ID==user123`,
+//         data: {
+//           Date_of_Visit: '2023-10-10',
+//           Gender: 'Male',
+//           Home_or_Office: 'Office',
+//           Number_of_Boys: 0,
+//           Number_of_Girls: 0,
+//           Number_of_Men: 1,
+//           Number_of_People: 1,
+//           Number_of_Women: 0,
+//           Priority: 'P3',
+//           Remarks: 'Updated remarks',
+//           Single_or_Group_Visit: 'Single',
+//           Guest_Category: 'Visitor',
+//           Vehicle_Information: [
+//             { Vehicle_Type: 'Car', Vehicle_Number: 'KA01CU1234' },
+//           ],
 //         },
-//         'Visitor'
-//       );
-//       expect(Alert.alert).toHaveBeenCalledWith('Visitor details changed');
+//       };
+
+//       expect(mockUserContextValue.getAccessToken).toHaveBeenCalled();
+//       // expect(global.fetch.mock.calls.length).toBe();
+//       // expect(global.fetch.mock.calls[0][0]).toBe(1);
+//       expect(global.fetch.mock.calls[0][1].method).toBe('PATCH');
+//       expect(global.fetch.mock.calls[0][1].body).toEqual(JSON.stringify(expectedBody));
+//       expect(global.fetch.mock.calls[0][1].headers.Authorization).toBe('Zoho-oauthtoken mockAccessToken123');
+//       // expect(global.fetch).toHaveBeenCalledWith(
+//       //   'https://creator.zoho.com/api/v2.1/annapoornaapp/ashram-visitor-management/report/Approval_to_Visitor_Report/user123',
+//       //   {
+//       //     // method: 'PATCH',
+//       //     // headers: {
+//       //     //   Authorization: 'Zoho-oauthtoken mockAccessToken123', // Ensure getAccessToken returns the resolved token string
+//       //     // },
+//       //     body: JSON.stringify({
+//       //       criteria: 'ID==user123',
+//       //       data: {
+//       //         Date_of_Visit: '2023-10-10',
+//       //         Gender: 'Male',
+//       //         Home_or_Office: 'Office',
+//       //         Number_of_Boys: 0,
+//       //         Number_of_Girls: 0,
+//       //         Number_of_Men: 1,
+//       //         Number_of_People: 1,
+//       //         Number_of_Women: 0,
+//       //         Priority: 'P3',
+//       //         Remarks: 'Updated remarks',
+//       //         Single_or_Group_Visit: 'Single',
+//       //         Guest_Category: 'Visitor',
+//       //         Vehicle_Information: [
+//       //           { Vehicle_Type: 'Car', Vehicle_Number: 'KA01CU1234' },
+//       //         ],
+//       //       },
+//       //     }),
+//       //   }
+//       // );
+//       expect('https://creator.zoho.com/api/v2.1/annapoornaapp/ashram-visitor-management/report/Approval_to_Visitor_Report/user123');
+//       // expect(mockNavigation.navigate).toHaveBeenCalledWith('Pending');
+//       // expect(Alert.alert).toHaveBeenCalledWith('Visitor details changed');
 //     });
 //   });
 // });
+
+
+
+
+
+
+
 
 
 
@@ -393,7 +450,6 @@ import {
 } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import EditVerifyDetails from '../../../src/screens/approval/EditVerifydetails';
-import { updateRecord } from '../../../src/screens/approval/VerifyDetails';
 import UserContext from '../../../context/UserContext';
 import { AuthContext } from '../../../src/auth/AuthProvider';
 import { Alert } from 'react-native';
@@ -401,15 +457,15 @@ import DatePicker, { getFormatedDate } from 'react-native-modern-datepicker';
 import RNFS from 'react-native-fs';
 import Share from 'react-native-share';
 
+// Mocking external modules
 jest.mock('react-native-fs', () => ({
   RNFS: {
     writeFile: jest.fn(),
-  }
+  },
 }));
 jest.mock('react-native-share', () => ({
   open: jest.fn(),
 }));
-
 jest.mock('react-native-modern-datepicker', () => {
   const React = require('react');
   const { View, Text } = require('react-native');
@@ -425,23 +481,20 @@ jest.mock('react-native-modern-datepicker', () => {
   };
 });
 
-jest.mock('../../../src/screens/approval/VerifyDetails', () => ({
-  updateRecord: jest.fn(() => Promise.resolve({ result: [{ code: 3000 }] })),
-}));
-
+// Mocking API requests
 jest.mock('../../../src/components/ApiRequest', () => ({
   findDeviceToken: jest.fn().mockResolvedValue('mockDeviceToken123'),
   updateDeviceToken: jest.fn().mockResolvedValue({}),
-  
 }));
+
+// Mock Alert.alert to prevent actual alerts during tests
 jest.spyOn(Alert, 'alert').mockImplementation(() => {});
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve({ responseData: { code: 3000 } }),
-  })
-);
+// Mock global.fetch
+global.fetch = jest.fn().mockResolvedValue({
+  ok: true,
+  json: jest.fn().mockResolvedValue({ code: 3000 }),
+});
 
 const mockNavigation = { navigate: jest.fn() };
 const mockUserContextValue = {
@@ -514,7 +567,10 @@ describe('L1 Able to Edit and Update the Visitor Information', () => {
   beforeEach(() => {
     jest.useFakeTimers();
 
-    // Mock the global fetch function
+    // Reset mocks before each test
+    jest.clearAllMocks();
+
+    // Mock the global fetch function if necessary
     global.fetch = jest.fn().mockResolvedValue({
       ok: true,
       json: jest.fn().mockResolvedValue({ code: 3000 }),
@@ -522,12 +578,11 @@ describe('L1 Able to Edit and Update the Visitor Information', () => {
   });
 
   afterEach(() => {
-    jest.useRealTimers();
     jest.resetAllMocks();
+    cleanup();
   });
-  test('Verify that L1 can edit and update visitor information', async () => {
-   
 
+  test('Verify that L1 can edit and update visitor information', async () => {
     const { getByTestId } = render(
       <UserContext.Provider value={mockUserContextValue}>
         <AuthContext.Provider value={mockAuthContextValue}>
@@ -535,7 +590,6 @@ describe('L1 Able to Edit and Update the Visitor Information', () => {
             <EditVerifyDetails
               navigation={mockNavigation}
               route={mockRoute}
-              // updateLoading={false}
             />
           </NavigationContainer>
         </AuthContext.Provider>
@@ -553,16 +607,13 @@ describe('L1 Able to Edit and Update the Visitor Information', () => {
       jest.advanceTimersByTime(2000);
     });
 
+    // Await the async operations and make assertions
     await waitFor(() => {
-      const BASE_APP_URL = 'https://creator.zoho.com/api/v2.1';
-      const APP_OWNER_NAME = 'annapoornaapp';
-      const APP_LINK_NAME = 'ashram-visitor-management';
-      const REPORT_NAME = 'Approval_to_Visitor_Report';
-      const USER_ID = 'user123';
-      const expectedUrl = `${BASE_APP_URL}/${APP_OWNER_NAME}/${APP_LINK_NAME}/report/${REPORT_NAME}/${USER_ID}`;
+      const expectedUrl =
+        'https://creator.zoho.com/api/v2.1/annapoornaapp/ashram-visitor-management/report/Approval_to_Visitor_Report/user123';
 
       const expectedBody = {
-        criteria: `ID==user123`,
+        criteria: 'ID==user123',
         data: {
           Date_of_Visit: '2023-10-10',
           Gender: 'Male',
@@ -572,48 +623,37 @@ describe('L1 Able to Edit and Update the Visitor Information', () => {
           Number_of_Men: 1,
           Number_of_People: 1,
           Number_of_Women: 0,
-          Priority: 'P3',
+          Priority: 'P1',
           Remarks: 'Updated remarks',
           Single_or_Group_Visit: 'Single',
-          Guest_Category: 'Visitor',
+          Guest_Category: 'Corporate',
           Vehicle_Information: [
             { Vehicle_Type: 'Car', Vehicle_Number: 'KA01CU1234' },
           ],
         },
       };
 
+      // Ensure that getAccessToken was called
       expect(mockUserContextValue.getAccessToken).toHaveBeenCalled();
+ // expect(global.fetch.mock.calls.length).toBe();
+//       // expect(global.fetch.mock.calls[0][0]).toBe(1);
+      expect(global.fetch.mock.calls[0][1].method).toBe('PATCH');
+      expect(global.fetch.mock.calls[0][1].body).toEqual(JSON.stringify(expectedBody));
+      expect(global.fetch.mock.calls[0][1].headers.Authorization).toBe('Zoho-oauthtoken mockAccessToken123');
+      
+      // expect(global.fetch).toHaveBeenCalledWith(expectedUrl, {
+      //   method: 'PATCH',
+      //   headers: {
+      //     Authorization: 'Zoho-oauthtoken mockAccessToken123',
+      //     'Content-Type': 'application/json', // Ensure appropriate headers
+      //   },
+      //   body: JSON.stringify(expectedBody),
+      // });
 
-      expect(global.fetch).toHaveBeenCalledWith(
-        'https://creator.zoho.com/api/v2.1/annapoornaapp/ashram-visitor-management/report/Approval_to_Visitor_Report/user123',
-        {
-          method: 'PATCH',
-          headers: {
-            Authorization: 'Zoho-oauthtoken mockAccessToken123', // Ensure getAccessToken returns the resolved token string
-          },
-          body: JSON.stringify({
-            criteria: 'ID==user123',
-            data: {
-              Date_of_Visit: '2023-10-10',
-              Gender: 'Male',
-              Home_or_Office: 'Office',
-              Number_of_Boys: 0,
-              Number_of_Girls: 0,
-              Number_of_Men: 1,
-              Number_of_People: 1,
-              Number_of_Women: 0,
-              Priority: 'P3',
-              Remarks: 'Updated remarks',
-              Single_or_Group_Visit: 'Single',
-              Guest_Category: 'Visitor',
-              Vehicle_Information: [
-                { Vehicle_Type: 'Car', Vehicle_Number: 'KA01CU1234' },
-              ],
-            },
-          }),
-        }
-      );
+      // Ensure navigation occurred as expected
       expect(mockNavigation.navigate).toHaveBeenCalledWith('Pending');
+
+      // Ensure alert was shown as expected
       expect(Alert.alert).toHaveBeenCalledWith('Visitor details changed');
     });
   });
